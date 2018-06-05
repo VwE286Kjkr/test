ba6facbc8acf7f47b1f5f67934a8f48705746d98
Johann Borg <j.borg@netrefer.com>
2018-06-05 16:30:57 +0200

    added login-page component to authentication library

--- libs/authentication/src/lib/authentication.module.ts
+++ libs/authentication/src/lib/authentication.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const authenticationRoutes: Route[] = [];
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [LoginPageComponent]
 })
 export class AuthenticationModule {}

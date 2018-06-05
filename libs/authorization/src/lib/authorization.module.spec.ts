3ba9b748d1a6d0bdaa8adf9e1f4774c05ce73782
Johann Borg <j.borg@netrefer.com>
2018-06-05 15:49:48 +0200

    added authorization library

---
+++ libs/authorization/src/lib/authorization.module.spec.ts
@@ -0,0 +1,16 @@
+import { async, TestBed } from '@angular/core/testing';
+import { AuthorizationModule } from './authorization.module';
+
+describe('AuthorizationModule', () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [AuthorizationModule]
+      }).compileComponents();
+    })
+  );
+
+  it('should create', () => {
+    expect(AuthorizationModule).toBeDefined();
+  });
+});

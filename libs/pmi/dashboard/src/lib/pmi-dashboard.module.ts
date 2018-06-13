21d928fc5efba141c234516a351a2a1b9e90d002
Johann Borg <j.borg@netrefer.com>
2018-06-13 16:34:36 +0200

    added material module to nrg dashboard & pmi dashboard

--- libs/pmi/dashboard/src/lib/pmi-dashboard.module.ts
+++ libs/pmi/dashboard/src/lib/pmi-dashboard.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+
+import { MaterialModule } from '@encore/material';
+
 @NgModule({
   imports: [
     CommonModule,
-
+    MaterialModule,
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ])

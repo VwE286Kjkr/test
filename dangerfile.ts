1ab15e8741c712656e6ae5c5be5ac65765be663f
Johann Borg <j.borg@netrefer.com>
2018-07-05 10:55:45 +0200

    added PR Assignee check to Dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,10 @@
-import {message, danger} from "danger"
+import { message, danger, fail } from "danger"
 
+const pr = danger.github.pr
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+// Always ensure we assign someone, so that our Slackbot can do its work correctly
+if (pr.assignee === null) {
+    fail("Please assign someone to merge this PR, and optionally include people who should review.")
+}

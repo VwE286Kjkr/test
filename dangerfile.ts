d07f39a200d6a6e2f99b7dba9bfea4ee838a09af
DUHbTPDeDQ <40567778+DUHbTPDeDQ@users.noreply.github.com>
2018-07-05 11:21:02 +0200

    Dangerfile update (#7)
    chore: Added PR Assignee check to Dangerfile. If assignee is missing, DangerJS will trigger a warning

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,10 @@
-import {message, danger} from "danger"
+import { message, danger, fail, warn } from "danger"
 
+const pr = danger.github.pr
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+// Always ensure we assign someone
+if (pr.assignee === null) {
+    warn("Please assign someone to merge this PR, and optionally include people who should review.")
+}
